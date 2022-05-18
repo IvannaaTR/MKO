c9e05cf4ed1c226448c3b03db0d04e4f5760c8d2
Ivanna Trybulovych <ivanna2292@gmail.com>
2022-05-18 21:17:17 +0300

    add files for task

---
+++ Trybulovych_MFE/Trybulovych_MFE/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace Trybulovych_MFE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txTextBox = new System.Windows.Forms.TextBox();
+            this.txLabel = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.N_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Uh0_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Uh1_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.fxTextBox = new System.Windows.Forms.TextBox();
+            this.uxTextBox = new System.Windows.Forms.TextBox();
+            this.fxLabel = new System.Windows.Forms.Label();
+            this.uxLabel = new System.Windows.Forms.Label();
+            this.bxTextBox = new System.Windows.Forms.TextBox();
+            this.bxLabel = new System.Windows.Forms.Label();
+            this.sxTextBox = new System.Windows.Forms.TextBox();
+            this.sxLabel = new System.Windows.Forms.Label();
+            this.functionGraphics = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.allGraphics = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGraphics)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.allGraphics)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txTextBox
+            // 
+            this.txTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txTextBox.Location = new System.Drawing.Point(86, 45);
+            this.txTextBox.Name = "txTextBox";
+            this.txTextBox.Size = new System.Drawing.Size(432, 28);
+            this.txTextBox.TabIndex = 38;
+            this.txTextBox.Text = "7*x";
+            // 
+            // txLabel
+            // 
+            this.txLabel.AutoSize = true;
+            this.txLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txLabel.Location = new System.Drawing.Point(7, 45);
+            this.txLabel.Name = "txLabel";
+            this.txLabel.Size = new System.Drawing.Size(70, 24);
+            this.txLabel.TabIndex = 37;
+            this.txLabel.Text = "T(x)  = ";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.PeachPuff;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.N_column,
+            this.Uh0_column,
+            this.Uh1_column});
+            this.dataGridView.Location = new System.Drawing.Point(616, 11);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(584, 298);
+            this.dataGridView.TabIndex = 36;
+            // 
+            // N_column
+            // 
+            this.N_column.HeaderText = "N";
+            this.N_column.MinimumWidth = 6;
+            this.N_column.Name = "N_column";
+            this.N_column.ReadOnly = true;
+            // 
+            // Uh0_column
+            // 
+            this.Uh0_column.HeaderText = "||uh0||";
+            this.Uh0_column.MinimumWidth = 6;
+            this.Uh0_column.Name = "Uh0_column";
+            this.Uh0_column.ReadOnly = true;
+            // 
+            // Uh1_column
+            // 
+            this.Uh1_column.HeaderText = "‖ uh1 ‖";
+            this.Uh1_column.MinimumWidth = 6;
+            this.Uh1_column.Name = "Uh1_column";
+            this.Uh1_column.ReadOnly = true;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalculateButton.Location = new System.Drawing.Point(12, 218);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(119, 48);
+            this.CalculateButton.TabIndex = 35;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // fxTextBox
+            // 
+            this.fxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fxTextBox.Location = new System.Drawing.Point(86, 162);
+            this.fxTextBox.Name = "fxTextBox";
+            this.fxTextBox.Size = new System.Drawing.Size(432, 28);
+            this.fxTextBox.TabIndex = 34;
+            this.fxTextBox.Text = "14*pi*(2*pi*x*sin(2*pi*x)-cos(2*pi*x))+x^2*(2*pi*cos(2*pi*x))+(1/(x)^2)*sin(2*pi*" +
+    "x)";
+            // 
+            // uxTextBox
+            // 
+            this.uxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxTextBox.Location = new System.Drawing.Point(86, 11);
+            this.uxTextBox.Name = "uxTextBox";
+            this.uxTextBox.Size = new System.Drawing.Size(432, 28);
+            this.uxTextBox.TabIndex = 32;
+            this.uxTextBox.Text = "sin(2*pi*x)";
+            // 
+            // fxLabel
+            // 
+            this.fxLabel.AutoSize = true;
+            this.fxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fxLabel.Location = new System.Drawing.Point(7, 162);
+            this.fxLabel.Name = "fxLabel";
+            this.fxLabel.Size = new System.Drawing.Size(62, 24);
+            this.fxLabel.TabIndex = 31;
+            this.fxLabel.Text = "f(x)  = ";
+            // 
+            // uxLabel
+            // 
+            this.uxLabel.AutoSize = true;
+            this.uxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxLabel.Location = new System.Drawing.Point(7, 11);
+            this.uxLabel.Name = "uxLabel";
+            this.uxLabel.Size = new System.Drawing.Size(64, 24);
+            this.uxLabel.TabIndex = 30;
+            this.uxLabel.Text = "u(x) = ";
+            // 
+            // bxTextBox
+            // 
+            this.bxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bxTextBox.Location = new System.Drawing.Point(86, 88);
+            this.bxTextBox.Name = "bxTextBox";
+            this.bxTextBox.Size = new System.Drawing.Size(432, 28);
+            this.bxTextBox.TabIndex = 42;
+            this.bxTextBox.Text = "x^2";
+            // 
+            // bxLabel
+            // 
+            this.bxLabel.AutoSize = true;
+            this.bxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bxLabel.Location = new System.Drawing.Point(7, 88);
+            this.bxLabel.Name = "bxLabel";
+            this.bxLabel.Size = new System.Drawing.Size(69, 24);
+            this.bxLabel.TabIndex = 41;
+            this.bxLabel.Text = "b(x)  = ";
+            // 
+            // sxTextBox
+            // 
+            this.sxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sxTextBox.Location = new System.Drawing.Point(86, 128);
+            this.sxTextBox.Name = "sxTextBox";
+            this.sxTextBox.Size = new System.Drawing.Size(432, 28);
+            this.sxTextBox.TabIndex = 40;
+            this.sxTextBox.Text = "1/(x^2)";
+            // 
+            // sxLabel
+            // 
+            this.sxLabel.AutoSize = true;
+            this.sxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sxLabel.Location = new System.Drawing.Point(7, 128);
+            this.sxLabel.Name = "sxLabel";
+            this.sxLabel.Size = new System.Drawing.Size(67, 24);
+            this.sxLabel.TabIndex = 39;
+            this.sxLabel.Text = "s(x)  = ";
+            // 
+            // functionGraphics
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.functionGraphics.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.functionGraphics.Legends.Add(legend3);
+            this.functionGraphics.Location = new System.Drawing.Point(11, 347);
+            this.functionGraphics.Name = "functionGraphics";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.functionGraphics.Series.Add(series3);
+            this.functionGraphics.Size = new System.Drawing.Size(573, 323);
+            this.functionGraphics.TabIndex = 43;
+            // 
+            // allGraphics
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.allGraphics.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.allGraphics.Legends.Add(legend4);
+            this.allGraphics.Location = new System.Drawing.Point(616, 347);
+            this.allGraphics.Name = "allGraphics";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.allGraphics.Series.Add(series4);
+            this.allGraphics.Size = new System.Drawing.Size(573, 323);
+            this.allGraphics.TabIndex = 44;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(1201, 682);
+            this.Controls.Add(this.allGraphics);
+            this.Controls.Add(this.functionGraphics);
+            this.Controls.Add(this.bxTextBox);
+            this.Controls.Add(this.bxLabel);
+            this.Controls.Add(this.sxTextBox);
+            this.Controls.Add(this.sxLabel);
+            this.Controls.Add(this.txTextBox);
+            this.Controls.Add(this.txLabel);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.fxTextBox);
+            this.Controls.Add(this.uxTextBox);
+            this.Controls.Add(this.fxLabel);
+            this.Controls.Add(this.uxLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.functionGraphics)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.allGraphics)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox txTextBox;
+        private System.Windows.Forms.Label txLabel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button CalculateButton;
+        public System.Windows.Forms.TextBox fxTextBox;
+        public System.Windows.Forms.TextBox uxTextBox;
+        private System.Windows.Forms.Label fxLabel;
+        private System.Windows.Forms.Label uxLabel;
+        public System.Windows.Forms.TextBox bxTextBox;
+        private System.Windows.Forms.Label bxLabel;
+        public System.Windows.Forms.TextBox sxTextBox;
+        private System.Windows.Forms.Label sxLabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart functionGraphics;
+        private System.Windows.Forms.DataVisualization.Charting.Chart allGraphics;
+        private System.Windows.Forms.DataGridViewTextBoxColumn N_column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Uh0_column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Uh1_column;
+    }
+}
+
---
+++ Trybulovych_MFE/Trybulovych_MFE/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using org.mariuszgromada.math.mxparser;
+
+
+namespace Trybulovych_MFE
+{
+    public partial class Form1 : Form
+    {
+        private int reload = 0;
+        public Function ux;
+        public Function Tx;
+        public Function bx;
+        public Function sx;
+        public Function fx;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            functionGraphics.ChartAreas[0].AxisX.Minimum = 0;
+            functionGraphics.ChartAreas[0].AxisX.Maximum = 1;
+            functionGraphics.Series[0].ChartType = SeriesChartType.Line;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void Initialize()
+        {
+            functionGraphics.Series[0].Points.Clear();
+            allGraphics.Series.Clear();
+            dataGridView.Rows.Clear();
+   
+        }
+       
+        public double ai_i(double previous_x, double x, double next_x, double h, Function Tx, Function bx, Function sx)
+        {
+            double previous_half = (previous_x + x) / 2.0;
+            double next_half = (next_x + x) / 2.0;
+            double result = (1 / h) * (Tx.calculate(previous_half) + Tx.calculate(next_half)) + 0.5*(bx.calculate(previous_half) - bx.calculate(next_half))+ (h/3)*(sx.calculate(previous_half) + sx.calculate(next_half));
+            return result;
+        }
+        public double ai_inext(double x, double next_x, double h, Function Tx, Function bx, Function sx)
+        {
+            double next_half = (next_x + x) / 2;
+            double result = (-1/h)*Tx.calculate(next_half)+ 0.5* bx.calculate(next_half)+(h/6)* sx.calculate(next_half);
+            return result;
+        } 
+        public double ainext_i(double x, double next_x, double h, Function Tx, Function bx, Function sx)
+        {
+            double next_half = (next_x + x) / 2;
+            double result = (-1 / h) * Tx.calculate(next_half)  -0.5 * bx.calculate(next_half) + (h / 6) * sx.calculate(next_half);
+            return result;
+        }
+        public double l_i(double previous_x, double x, double next_x, double h, Function fx)
+        {
+            double previous_half = (previous_x + x) / 2;
+            double next_half = (next_x + x) / 2;
+            double result = (h/2)*(fx.calculate(previous_half)+ fx.calculate(next_half));
+            return result;
+        }
+
+        public List<double> SLAR(List<double> x, Function Tx, Function bx, Function sx, Function fx, double h, int n)
+        {
+            List<double> c_upper = new List<double>();
+            List<double> b_middle = new List<double>();
+            List<double> a_lower = new List<double>();
+            List<double> l = new List<double>();
+            List<double> q = new List<double>();
+
+            for (int i=1; i<n-1; i++)
+            {
+                a_lower.Add(ainext_i(x[i], x[i+1], h, Tx, bx, sx));
+                b_middle.Add(ai_i(x[i - 1], x[i], x[i + 1],h, Tx, bx, sx));
+                c_upper.Add(ai_inext(x[i], x[i + 1], h, Tx, bx, sx));
+            }
+            b_middle.Add(ai_i(x[x.Count()-3], x[x.Count() - 2], x[x.Count() - 1], h, Tx, bx, sx));
+         
+            for (int i = 1; i < n; i++)
+            {
+                l.Add(l_i(x[i - 1], x[i], x[i+1], h, fx));
+            }
+
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                q.Add(0);
+            }
+            n = b_middle.Count();
+            a_lower.Insert(0,0);
+            c_upper.Add(0);
+            c_upper[0] /= b_middle[0];
+            l[0] /= b_middle[0];
+            for (int i = 1; i < n; i++)
+            {
+                double temporary = 1 / (b_middle[i] - c_upper[i-1] * a_lower[i]);
+                c_upper[i] *= temporary;
+                l[i] = (l[i] - l[i - 1] * a_lower[i]) * temporary;
+            }
+            q[n - 1] = l[n - 1];
+
+            for(int i=n-2; i>=0; i--)
+            {
+                q[i] = l[i] - (c_upper[i]*q[i+1]);
+            }
+            q.Insert(0, 0);
+            
+            return q;
+        }
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            if (reload != 0)
+            {
+                Initialize();
+            }
+            reload++;
+
+            ux = new Function("f(x) = " + uxTextBox.Text);
+            Tx = new Function("f(x) = " + txTextBox.Text);
+            bx = new Function("f(x) = " + bxTextBox.Text);
+            sx = new Function("f(x) = " + sxTextBox.Text);
+            fx = new Function("f(x) = " + fxTextBox.Text);
+         
+            int N= 4;
+            int N_max = 100;
+
+            while (N <= N_max)
+            {
+                
+                var xi = new List<double>();
+                for (double i = 0; i <= N; i++)
+                {
+                    xi.Add((1.0 / N) * i);
+                }
+                var h = xi[1] - xi[0];
+                var qi = SLAR(xi, Tx, bx, sx, fx, h, N);
+                var uh = new List<double>();
+
+
+                uh.Add(0);
+                for (int i = 1; i < N; i++)
+                {
+                    var kurant = 0.0;
+                    var x = xi[i];
+
+                    if (x > xi[i - 1] && x <= xi[i])
+                    {
+                        kurant = (x - xi[i - 1]) / h;
+                    }
+                    else if (x > xi[i] && x <= xi[i + 1])
+                    {
+                        kurant = (xi[i + 1] - x) / h;
+                    }
+                    else
+                    {
+                        kurant = 0;
+                    }
+
+                    uh.Add(qi[i] * kurant);
+
+                }
+                uh.Add(0);
+
+                double uh0 = 0;
+                for (int i = 0; i < qi.Count() - 1; i++)
+                {
+                    uh0 += Math.Pow(qi[i], 2) + qi[i] * qi[i + 1] + Math.Pow(qi[i + 1], 2);
+                }
+                uh0 = Math.Sqrt((h / 3) * uh0);
+
+                double uh1 = 0;
+                for (int i = 0; i < qi.Count() - 1; i++)
+                {
+                    uh1 += Math.Pow(qi[i] - qi[i + 1], 2);
+                }
+                uh1 = Math.Sqrt((uh1/h) + Math.Pow(uh0, 2));
+
+                dataGridView.Rows.Add(N, Math.Round(uh0, 5), Math.Round(uh1, 5));
+
+                allGraphics.Series.Add($"N={N}");
+                allGraphics.ChartAreas[0].AxisX.Minimum = 0;
+                allGraphics.ChartAreas[0].AxisX.Maximum = 1;
+                allGraphics.Series[$"N={N}"].ChartType = SeriesChartType.Line;
+                for (int i = 0; i < uh.Count; i++)
+                {
+                    allGraphics.Series[$"N={N}"].Points.AddXY(xi[i], uh[i]);
+                }
+
+                if (N >= 10)
+                {
+                    N += 10;
+                }
+                else
+                {
+                    N += 2;
+                }
+                
+            }
+            for (double i = 0.0; i <= 1; i += 0.001)
+            {
+                functionGraphics.Series[0].Points.AddXY(i, ux.calculate(i));
+            }
+        }
+
+     
+    }
+}
